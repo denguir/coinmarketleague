5ddcb72374b605addab17a2b35644f0ffa439290
denguir <anass.den@gmail.com>
2021-05-02 16:34:31 +0200

    Https redirect

--- coinmarketleague/settings.py
+++ coinmarketleague/settings.py
@@ -154,6 +154,13 @@ else:
     DEFAULT_FROM_EMAIL= os.environ.get('EMAIL_ADDRESS')
 
     # HTTPS
+    MIDDLEWARE = [
+    # SecurityMiddleware must be listed before other middleware
+    'django.middleware.security.SecurityMiddleware',
+    ]   
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
     CSRF_COOKIE_SECURE=True
     SESSION_COOKIE_SECURE=True
 
