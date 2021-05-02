4ef0028f4d19a8b8090a0cb3fa3e6051903e3f42
denguir <anass.den@gmail.com>
2021-05-02 17:05:27 +0200

    settings update

--- coinmarketleague/settings.py
+++ coinmarketleague/settings.py
@@ -141,18 +141,18 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home_out'
 
 
-# Server settings
-if DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # for dev
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST')
-    EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
-    EMAIL_USE_TLS = int(os.environ.get("EMAIL_USE_TLS", default=1))
-    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-    DEFAULT_FROM_EMAIL= os.environ.get('EMAIL_ADDRESS')
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT'))
+EMAIL_USE_TLS = int(os.environ.get("EMAIL_USE_TLS", default=1))
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+DEFAULT_FROM_EMAIL= os.environ.get('EMAIL_ADDRESS')
 
+# Server settings
+
+if not DEBUG:
     # HTTPS
     MIDDLEWARE = [
     # SecurityMiddleware must be listed before other middleware
